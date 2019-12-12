15d77f645c5a1a3433febcc4b59a41f41edcbe8e
mr-craftsman <radesign@op.pl>
2019-12-12 11:06:39 +0100

    Add files via upload

---
+++ js/zadanie06.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function(readyList) 
+    {
+        var readyList = document.getElementById("shopping-list");
+        document.getElementById("button-1").addEventListener("click", function(newContent)
+        {
+            var newContent = document.createElement("li");
+            readyList.appendChild(newContent).innerHTML = "Chleb";
+        });
+        document.getElementById("button-2").addEventListener("click", function()
+        {
+            
+            readyList.removeChild(readyList.childNodes[readyList.childNodes.length-1]); 
+
+        });
+        document.getElementById("button-3").addEventListener("click", function(duplicate)
+        {
+            var duplicate = readyList.children[1].cloneNode(true);
+            readyList.appendChild(duplicate);
+        });
+
+    })
